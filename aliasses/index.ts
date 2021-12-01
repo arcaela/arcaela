f8a1edafa3bd19139b3a90282ab421acd1e1556f
Alejandro Reyes <arcaela.reyes@gmail.com>
2021-12-01 14:21:18 -0400

    Switch Aliasses to TypeScript

--- aliasses/index.ts
+++ aliasses/index.ts
@@ -1,126 +1,103 @@
 'use strict'
 
-var BuiltinModule = require('module')
+const __path = require("path");
+const __main = '_simulateRepl' in require.main ? undefined : require.main;
+const __module = module.constructor.length ? module.constructor : require("module");
+const __store = {
+    paths:[],
+    alias:{},
+    aliasNames:[],
+};
 
-var Module = module.constructor.length > 1
-  ? module.constructor
-  : BuiltinModule
 
-var nodePath = require('path')
-
-var modulePaths = []
-var moduleAliases = {}
-var moduleAliasNames = []
-
-var oldNodeModulePaths = Module._nodeModulePaths
-Module._nodeModulePaths = function (from) {
-  var paths = oldNodeModulePaths.call(this, from)
-  if (from.indexOf('node_modules') === -1)
-    paths = modulePaths.concat(paths)
-  return paths
+let __nodeModulePaths = __module._nodeModulePaths;
+__module._nodeModulePaths = function _nodeModulePaths(src){
+    let paths = __nodeModulePaths.call(this, src);
+    if(src.indexOf("node_modules")<0)
+        paths = __store.paths.concat( paths );
+    return paths;
 }
-var oldResolveFilename = Module._resolveFilename
-Module._resolveFilename = function (request, parentModule, isMain, options) {
-  for (var i = moduleAliasNames.length; i-- > 0;) {
-    var alias = moduleAliasNames[i]
-    if (isPathMatchesAlias(request, alias)) {
-      var aliasTarget = moduleAliases[alias]
-      if (typeof moduleAliases[alias] === 'function') {
-        var fromPath = parentModule.filename
-        aliasTarget = moduleAliases[alias](fromPath, request, alias)
-        if (!aliasTarget || typeof aliasTarget !== 'string') {
-          throw new Error('[module-alias] Expecting custom handler function to return path.')
+
+let __resolveFilename = __module._resolveFilename;
+__module._resolveFilename = function _resolveFilename(req, parent, main: boolean, opts: object){
+    for(let alias of __store.aliasNames){
+        if(isAlias(req,alias)){
+            let target = __store.alias[ alias ];
+            if(typeof target === 'function'){
+                let from = parent.filename;
+                target = target(from, req, alias);
+                if(!target || typeof target!=='string')
+					throw new Error('[module-alias] Expecting custom handler function to return path.')
+            }
+            req = __path.join(target, req.substr(alias.length));
+            break;
         }
-      }
-      request = nodePath.join(aliasTarget, request.substr(alias.length))
-      break
     }
-  }
-  return oldResolveFilename.call(this, request, parentModule, isMain, options)
-}
-function isPathMatchesAlias (path, alias) {
-  if (path.indexOf(alias) === 0) {
-    if (path.length === alias.length) return true
-    if (path[alias.length] === '/') return true
-  }
-  return false
-}
-function addPathHelper (path, targetArray) {
-  path = nodePath.normalize(path)
-  if (targetArray && targetArray.indexOf(path) === -1)
-    targetArray.unshift(path)
-}
-function removePathHelper (path, targetArray) {
-  if (targetArray) {
-    var index = targetArray.indexOf(path)
-    if (index !== -1)
-      targetArray.splice(index, 1)
-  }
-}
-function addPath (path) {
-  var parent
-  path = nodePath.normalize(path)
-  if (modulePaths.indexOf(path) === -1) {
-    modulePaths.push(path)
-    var mainModule = getMainModule()
-    if (mainModule)
-      addPathHelper(path, mainModule.paths)
-    parent = module.parent
-    while (parent && parent !== mainModule) {
-      addPathHelper(path, parent.paths)
-      parent = parent.parent
-    }
-  }
-}
-function getMainModule () {
-  return '_simulateRepl' in require.main ? undefined : require.main
+    return __resolveFilename.call(this, req, parent, main, opts);
 }
 
-/**
- * @example
- * addAliasses({
- *  "js":__dirname + "/dist/js/",
- *  "css":__dirname + "/dist/css/",
- * });
- * 
- * require("js/index");
- * require("css/index.css");
- * 
- * @param aliases - Add aliasses as Object Alias
- */
-export function addAliases<A extends Record<string, string>> (aliases: A) : void {
-  for (var alias in aliases) {
-    addAlias(alias, aliases[alias])
-  }
+function isAlias(path: string, alias: string) : boolean {
+    return path.indexOf(alias)===0 && (
+        path.length===alias.length || path[ alias.length ]==='/'
+    );
 }
-/**
- * 
- * @param {string} alias 
- * @param {string} target 
- */
-export function addAlias (alias: string, target: string) : void {
-  moduleAliases[alias] = target
-  moduleAliasNames = Object.keys(moduleAliases)
-  moduleAliasNames.sort()
-}
-/**
- * Remove all aliasses from registry.
- */
-export function reset () : void {
-  var mainModule = getMainModule()
-  modulePaths.forEach(function (path) {
-    if (mainModule) removePathHelper(path, mainModule.paths)
-    Object.getOwnPropertyNames(require.cache).forEach(function (name) {
-      if (name.indexOf(path) !== -1)
-        delete require.cache[name]
-    })
-    var parent = module.parent
-    while (parent && parent !== mainModule) {
-      removePathHelper(path, parent.paths)
-      parent = parent.parent
+
+function removeAlias(path: string, target: string[]) : void {
+    if(target){
+        let i = target.indexOf(path);
+        if(i>=0) target.splice(i,1);
     }
-  })
-  modulePaths = []
-  moduleAliases = {}
-  moduleAliasNames = []
 }
+
+namespace Arcaela {
+    export module aliasses {
+        /**
+         * 
+         * @param {string} alias 
+         * @param {string} target 
+         */
+        export function addAlias(alias: string, target: string){
+            __store.alias[ alias ] = target;
+            __store.aliasNames = Object.keys( __store.alias );
+            __store.aliasNames = __store.aliasNames.sort();
+        }
+        /**
+         * @example
+         * addAliasses({
+         *  "js":__dirname + "/dist/js/",
+         *  "css":__dirname + "/dist/css/",
+         * });
+         * 
+         * require("js/index");
+         * require("css/index.css");
+         * 
+         * @param alias - Add aliasses as Object Alias
+         */
+        export function addAliasses(alias: Record<string, string>){
+            for(let a in alias)
+                addAlias(a, alias[a]);
+        }
+        /**
+         * Remove all aliasses from registry.
+         */
+        export function reset () : void {
+            __store.paths.forEach(function (path) {
+                if (__main) removeAlias(path, __main.paths);
+                Object.getOwnPropertyNames(require.cache).forEach(function (name) {
+                    if (name.indexOf(path) !== -1)
+                        delete require.cache[name]
+                })
+                var parent = module.parent
+                while (parent && parent !== __main) {
+                    removeAlias(path, parent.paths)
+                    parent = parent.parent
+                }
+            });
+            __store.paths = [];
+            __store.alias = {};
+            __store.aliasNames = [];
+        }
+    }
+}
+
+export = Arcaela.aliasses;
